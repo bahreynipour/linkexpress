79a46623321a5ff84acd5b80a476415d198bc75d
bahreynipour <bahreynipour@gmail.com>
2024-02-29 21:38:11 +0330

    [fix] revert installing_key constant back

--- app/Constants.php
+++ app/Constants.php
@@ -4,41 +4,54 @@ namespace LinkExpress;
 
 use LinkExpress\Concerns\HasConstants;
 
-class Constants {
-    use HasConstants;
+class Constants
+{
+	use HasConstants;
 
-    public static function init( $appName, $appVersion ) {
-        self::setConstant( [
-            'appName'                => $appName,
-            'appVersion'             => $appVersion,
-            'appVersionKey'         => $appName . '_version',
-            'dbVersionKey'          => $appName . '_db_version',
-            'activationRedirectKey' => $appName . '_activation_redirect',
-            'settingsSlug'            => $appName . '-settings'
-        ] );
-    }
+	public static function init($appName, $appVersion)
+	{
+		self::setConstant([
+			'appName' => $appName,
+			'appVersion' => $appVersion,
+			'appVersionKey' => $appName . '_version',
+			'dbVersionKey' => $appName . '_db_version',
+			'installing_key' => $appName . '_installing',
+			'activationRedirectKey' => $appName . '_activation_redirect',
+			'settingsSlug' => $appName . '-settings'
+		]);
+	}
 
-    public static function appName() {
-        return self::getConstant( 'appName' );
-    }
+	public static function appName()
+	{
+		return self::getConstant('appName');
+	}
 
-    public static function appVersion() {
-        return self::getConstant( 'appVersion' );
-    }
+	public static function appVersion()
+	{
+		return self::getConstant('appVersion');
+	}
 
-    public static function appVersionKey() {
-        return self::getConstant( 'appVersionKey' );
-    }
+	public static function appVersionKey()
+	{
+		return self::getConstant('appVersionKey');
+	}
 
-    public static function dbVersionKey() {
-        return self::getConstant( 'dbVersionKey' );
-    }
+	public static function dbVersionKey()
+	{
+		return self::getConstant('dbVersionKey');
+	}
 
-    public static function activationRedirectKey() {
-        return self::getConstant( 'activationRedirectKey' );
-    }
+	public static function activationRedirectKey()
+	{
+		return self::getConstant('activationRedirectKey');
+	}
 
-    public static function settingsSlug() {
-        return self::getConstant( 'settingsSlug' );
-    }
+	public static function installingKey() {
+		return self::getConstant( 'installing_key' );
+	}
+
+	public static function settingsSlug()
+	{
+		return self::getConstant('settingsSlug');
+	}
 }
